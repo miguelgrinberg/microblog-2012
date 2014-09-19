6d80ff778f39791610734b8d65b2c93a6e593b61
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-09-19 07:47:13 -0700

    python 3, pep8 and other various updates

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
-    remember_me = BooleanField('remember_me', default = False)
-    
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
