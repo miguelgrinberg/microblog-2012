27e49bdb637456fc058b69fb5bcebd8e679dd7b0
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-09-19 07:47:13 -0700

    python 3, pep8 and other various updates

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, \
+    MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -19,19 +20,22 @@ if not app.debug:
     credentials = None
     if MAIL_USERNAME or MAIL_PASSWORD:
         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT),
+                               'no-reply@' + MAIL_SERVER, ADMINS,
+                               'microblog failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a',
+                                       1 * 1024 * 1024, 10)
     file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.INFO)
     app.logger.info('microblog startup')
 
 from app import views, models
-
