26ee4e837c9a4b38d06a3b31b29ad0adcdd1512f
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-09-19 07:47:13 -0700

    python 3, pep8 and other various updates

--- app/views.py
+++ app/views.py
@@ -1,21 +1,22 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' }
+    user = {'nickname': 'Miguel'}
     posts = [
-        { 
-            'author': { 'nickname': 'John' }, 
-            'body': 'Beautiful day in Portland!' 
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
-        { 
-            'author': { 'nickname': 'Susan' }, 
-            'body': 'The Avengers movie was so cool!' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template("index.html",
-        title = 'Home',
-        user = user,
-        posts = posts)
+                           title='Home',
+                           user=user,
+                           posts=posts)
