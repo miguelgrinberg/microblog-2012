4d1cca34078b24932d78946392694d15a0345625
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-09-19 07:47:13 -0700

    python 3, pep8 and other various updates

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from flask.ext.login import login_user, logout_user, current_user, \
+    login_required
 from datetime import datetime
+from app import app, db, lm, oid
+from .forms import LoginForm, EditForm
+from .models import User
+
 
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 @app.before_request
 def before_request():
@@ -17,14 +20,17 @@ def before_request():
         db.session.add(g.user)
         db.session.commit()
 
+
 @app.errorhandler(404)
-def internal_error(error):
+def not_found_error(error):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
+
 
 @app.route('/')
 @app.route('/index')
@@ -32,21 +38,22 @@ def internal_error(error):
 def index():
     user = g.user
     posts = [
-        { 
-            'author': { 'nickname': 'John' }, 
-            'body': 'Beautiful day in Portland!' 
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
-        { 
-            'author': { 'nickname': 'Susan' }, 
-            'body': 'The Avengers movie was so cool!' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template('index.html',
-        title = 'Home',
-        user = user,
-        posts = posts)
+                           title='Home',
+                           user=user,
+                           posts=posts)
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
 def login():
     if g.user is not None and g.user.is_authenticated():
@@ -54,24 +61,25 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-    return render_template('login.html', 
-        title = 'Sign In',
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
 
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
         flash('Invalid login. Please try again.')
         return redirect(url_for('login'))
-    user = User.query.filter_by(email = resp.email).first()
+    user = User.query.filter_by(email=resp.email).first()
     if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
         nickname = User.make_unique_nickname(nickname)
-        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
         # make the user follow him/herself
@@ -81,30 +89,33 @@ def after_login(resp):
     if 'remember_me' in session:
         remember_me = session['remember_me']
         session.pop('remember_me', None)
-    login_user(user, remember = remember_me)
+    login_user(user, remember=remember_me)
     return redirect(request.args.get('next') or url_for('index'))
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
-    
+
+
 @app.route('/user/<nickname>')
 @login_required
 def user(nickname):
-    user = User.query.filter_by(nickname = nickname).first()
-    if user == None:
-        flash('User ' + nickname + ' not found.')
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
         return redirect(url_for('index'))
     posts = [
-        { 'author': user, 'body': 'Test post #1' },
-        { 'author': user, 'body': 'Test post #2' }
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html',
-        user = user,
-        posts = posts)
+                           user=user,
+                           posts=posts)
 
-@app.route('/edit', methods = ['GET', 'POST'])
+
+@app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
     form = EditForm(g.user.nickname)
@@ -118,36 +129,36 @@ def edit():
     elif request.method != "POST":
         form.nickname.data = g.user.nickname
         form.about_me.data = g.user.about_me
-    return render_template('edit.html',
-        form = form)
+    return render_template('edit.html', form=form)
+
 
 @app.route('/follow/<nickname>')
 def follow(nickname):
-    user = User.query.filter_by(nickname = nickname).first()
-    if user == None:
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
         flash('User ' + nickname + ' not found.')
         return redirect(url_for('index'))
     u = g.user.follow(user)
     if u is None:
         flash('Cannot follow ' + nickname + '.')
-        return redirect(url_for('user', nickname = nickname))
+        return redirect(url_for('user', nickname=nickname))
     db.session.add(u)
     db.session.commit()
     flash('You are now following ' + nickname + '!')
-    return redirect(url_for('user', nickname = nickname))
+    return redirect(url_for('user', nickname=nickname))
+
 
 @app.route('/unfollow/<nickname>')
 def unfollow(nickname):
-    user = User.query.filter_by(nickname = nickname).first()
-    if user == None:
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
         flash('User ' + nickname + ' not found.')
         return redirect(url_for('index'))
     u = g.user.unfollow(user)
     if u is None:
         flash('Cannot unfollow ' + nickname + '.')
-        return redirect(url_for('user', nickname = nickname))
+        return redirect(url_for('user', nickname=nickname))
     db.session.add(u)
     db.session.commit()
     flash('You have stopped following ' + nickname + '.')
-    return redirect(url_for('user', nickname = nickname))
-
+    return redirect(url_for('user', nickname=nickname))
--- tests.py
+++ tests.py
@@ -7,93 +7,100 @@ from config import basedir
 from app import app, db
 from app.models import User, Post
 
+
 class TestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
-        app.config['CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(basedir, 'test.db')
         db.create_all()
-        
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
     def test_avatar(self):
         # create a user
-        u = User(nickname = 'john', email = 'john@example.com')
+        u = User(nickname='john', email='john@example.com')
         avatar = u.avatar(128)
-        expected = 'http://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6'
+        expected = 'http://www.gravatar.com/avatar/' + \
+            'd4c74594d841139328695756648b6bd6'
         assert avatar[0:len(expected)] == expected
 
     def test_make_unique_nickname(self):
         # create a user and write it to the database
-        u = User(nickname = 'john', email = 'john@example.com')
+        u = User(nickname='john', email='john@example.com')
         db.session.add(u)
         db.session.commit()
         nickname = User.make_unique_nickname('john')
         assert nickname != 'john'
         # make another user with the new nickname
-        u = User(nickname = nickname, email = 'susan@example.com')
+        u = User(nickname=nickname, email='susan@example.com')
         db.session.add(u)
         db.session.commit()
         nickname2 = User.make_unique_nickname('john')
         assert nickname2 != 'john'
         assert nickname2 != nickname
-        
+
     def test_follow(self):
-        u1 = User(nickname = 'john', email = 'john@example.com')
-        u2 = User(nickname = 'susan', email = 'susan@example.com')
+        u1 = User(nickname='john', email='john@example.com')
+        u2 = User(nickname='susan', email='susan@example.com')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        assert u1.unfollow(u2) == None
+        assert u1.unfollow(u2) is None
         u = u1.follow(u2)
         db.session.add(u)
         db.session.commit()
-        assert u1.follow(u2) == None
+        assert u1.follow(u2) is None
         assert u1.is_following(u2)
         assert u1.followed.count() == 1
         assert u1.followed.first().nickname == 'susan'
         assert u2.followers.count() == 1
         assert u2.followers.first().nickname == 'john'
         u = u1.unfollow(u2)
-        assert u != None
+        assert u is not None
         db.session.add(u)
         db.session.commit()
-        assert u1.is_following(u2) == False
+        assert not u1.is_following(u2)
         assert u1.followed.count() == 0
         assert u2.followers.count() == 0
-        
+
         def test_follow_posts(self):
             # make four users
-            u1 = User(nickname = 'john', email = 'john@example.com')
-            u2 = User(nickname = 'susan', email = 'susan@example.com')
-            u3 = User(nickname = 'mary', email = 'mary@example.com')
-            u4 = User(nickname = 'david', email = 'david@example.com')
+            u1 = User(nickname='john', email='john@example.com')
+            u2 = User(nickname='susan', email='susan@example.com')
+            u3 = User(nickname='mary', email='mary@example.com')
+            u4 = User(nickname='david', email='david@example.com')
             db.session.add(u1)
             db.session.add(u2)
             db.session.add(u3)
             db.session.add(u4)
             # make four posts
             utcnow = datetime.utcnow()
-            p1 = Post(body = "post from john", author = u1, timestamp = utcnow + timedelta(seconds = 1))
-            p2 = Post(body = "post from susan", author = u2, timestamp = utcnow + timedelta(seconds = 2))
-            p3 = Post(body = "post from mary", author = u3, timestamp = utcnow + timedelta(seconds = 3))
-            p4 = Post(body = "post from david", author = u4, timestamp = utcnow + timedelta(seconds = 4))
+            p1 = Post(body="post from john", author=u1,
+                      timestamp=utcnow + timedelta(seconds=1))
+            p2 = Post(body="post from susan", author=u2,
+                      timestamp=utcnow + timedelta(seconds=2))
+            p3 = Post(body="post from mary", author=u3,
+                      timestamp=utcnow + timedelta(seconds=3))
+            p4 = Post(body="post from david", author=u4,
+                      timestamp=utcnow + timedelta(seconds=4))
             db.session.add(p1)
             db.session.add(p2)
             db.session.add(p3)
             db.session.add(p4)
             db.session.commit()
             # setup the followers
-            u1.follow(u1) # john follows himself
-            u1.follow(u2) # john follows susan
-            u1.follow(u4) # john follows david
-            u2.follow(u2) # susan follows herself
-            u2.follow(u3) # susan follows mary
-            u3.follow(u3) # mary follows herself
-            u3.follow(u4) # mary follows david
-            u4.follow(u4) # david follows himself
+            u1.follow(u1)  # john follows himself
+            u1.follow(u2)  # john follows susan
+            u1.follow(u4)  # john follows david
+            u2.follow(u2)  # susan follows herself
+            u2.follow(u3)  # susan follows mary
+            u3.follow(u3)  # mary follows herself
+            u3.follow(u4)  # mary follows david
+            u4.follow(u4)  # david follows himself
             db.session.add(u1)
             db.session.add(u2)
             db.session.add(u3)
@@ -112,6 +119,7 @@ class TestCase(unittest.TestCase):
             assert f2 == [p3, p2]
             assert f3 == [p4, p3]
             assert f4 == [p4]
-        
+
+
 if __name__ == '__main__':
     unittest.main()
