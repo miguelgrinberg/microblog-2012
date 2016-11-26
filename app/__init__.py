0fbffd594b51712d6c7b6e145d98dc17441bdbfb
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-11-25 16:06:32 -0800

    updated flask.ext imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_openid import OpenID
 from config import basedir
 
 app = Flask(__name__)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, \
-    login_required
+from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
